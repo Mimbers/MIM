46e399ffb0bf7f06cb1ee4a92592a137b37340d8
Jack <jhlee409@naver.com>
2022-11-03 23:46:30 +0900

    feat: jest 절대 경로 세팅 #3

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 
 import styles from "@/pages/index.module.scss";
-import Home from "../src/components/home/Home.tsx";
+
+import Home from "@/components/Home/Home.tsx";
 
 export default function HomePage() {
   return <Home />;
