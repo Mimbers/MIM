42b8c7d2841666b5d288177ebe8eaca4a44f2850
Jack <jhlee409@naver.com>
2022-11-03 23:52:32 +0900

    feat: Image 컴포넌트 사용 세팅 #5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const nextConfig = {
   sassOptions: {
+    images: {
+      loader: "akamai",
+      pathL: "/",
+    },
     includePaths: [path.join(__dirname, "styles")],
     prependData: `@import "styles/_variables.scss"; @import "styles/_variables.scss";`,
   },
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,10 +1,11 @@
 import styles from "./Home.module.scss";
-
+import Image from "next/image";
 const Home = () => {
   return (
     <div className={styles.container}>
       <h1>MIM 플젝 시작</h1>
       <p>경축</p>
+      <Image src="/vercel.svg" width={200} height={200} alt="logo" />
     </div>
   );
 };
