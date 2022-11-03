25685ee150a9a81ed7c23c0aebcf1c930fe7d67c
Jack <jhlee409@naver.com>
2022-11-03 23:41:58 +0900

    feat: 디렉토리 구조 세팅 #3

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,12 @@
+import styles from "./Home.module.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <h1>MIM 플젝 시작</h1>
+      <p>경축</p>
+    </div>
+  );
+};
+
+export default Home;
