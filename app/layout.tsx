42de3cbc15b9bb97ccea5707762ea4fe3c19dd7c
Jack <jhlee409@naver.com>
2022-12-06 22:09:17 +0900

    fix: 페이지 경로 수정 (#8)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import Sidebar from "@/components/layout/sidebar/Sidebar.tsx";
+import Sidebar from "@/components/layout/sidebar/Sidebar";
+import "../styles/global.css";
 
 export default function RootLayout({
   children,
